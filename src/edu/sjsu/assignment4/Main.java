f22eeed401cdd09b4115eaebf98fa94156513dcb
lilou <lilou.sicard-noel@sjsu.edu>
2021-07-22 11:02:49 -0700

    commit to add all files toward final project

--- src/edu/sjsu/assignment4/Main.java
+++ src/edu/sjsu/assignment4/Main.java
@@ -1,7 +1,8 @@
 package edu.sjsu.assignment4;
 
 public class Main {
-
+    //javadoc website
+    //https://lilousicard.github.io/assignment4_LSN/
     public static void main(String[] args) {
         AppointmentProgram.start();
     }
